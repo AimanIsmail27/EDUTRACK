98a05669d4e7dd4630024412802717ba58077e54
AimanIsmail27 <165313880+AimanIsmail27@users.noreply.github.com>
2025-12-29 11:45:41 +0800

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-
+//s
 class StudentController extends Controller
 {
     /**
